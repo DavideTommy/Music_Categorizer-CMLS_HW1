e22a5e2f4e4e54e9ee3029027ed16773446ba1b3
Davide Lorenzi <davide.lorenzi.96@gmail.com>
2021-04-26 20:05:18 +0200

    update of .py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import librosa
+import librosa.display
 import os
 import matplotlib.pyplot as plt
 import sklearn.svm #containts the class that implements the machine to analyze music
@@ -9,7 +10,7 @@ import scipy as sp
 
 # we skip the preliminary elaboration of the signal
 def compute_mfcc(audio, fs, n_mfcc):
-    # Compute the spectrogram of the audio signal. We take the magnitude od the signal, windowing it
+    # Compute the spectrogram of the audio signal. We take the magnitude of the signal, windowing it
     X = np.abs(librosa.stft(
         audio,
         window='hamming',
@@ -35,48 +36,98 @@ def compute_mfcc(audio, fs, n_mfcc):
 
     # Apply the DCT to log melspectrogram to obtain the coefficients
     mfcc = sp.fftpack.dct(log_melspectrogram, axis=0, norm='ortho')[1:n_mfcc + 1]
-    return mfcc
+    return mfcc  # n_mfcc x no. of windows
 
 
 classes = ['rock', 'pop', 'reggae', 'disco']  # now I wanna train my algorithm.
-n_mfcc = 13
+
+# we compute the number of factors for each feature
+n_mfcc = 35
+n_chroma = 12
+n_spec_roll = 1
+n_spec_centr = 1
+n_feat_weights = n_mfcc + n_chroma + n_spec_roll + n_spec_centr
 dict_train_features = {'rock': [], 'pop': [], 'reggae': [], 'disco': []}
+mfcc_plot = {'rock': [], 'pop': [], 'reggae': [], 'disco': []}  # for plotting the MFCC figures
 
 for c in classes:
-    print("Sto analizzando " + '{}'.format(c))
-    train_root = 'D:/Documenti/SampleCanzoni/PreProcessed/Selected/{}/train/'.format(c)
-    class_train_files = [f for f in os.listdir(train_root) if f.endswith('.mp3')]  # all files .mp3
+    # print("Sto analizzando " + '{}'.format(c))
+    train_root = '/Users/matteosartori/opt/miniconda3/CMLS/PreProcessedRed/{}/train'.format(c)
+    class_train_files = [f for f in os.listdir(train_root) if f.endswith('.mp3')]
     n_train_samples = len(class_train_files)
 
-    train_features = np.zeros(
-        (n_train_samples, n_mfcc))  # we initialize the metrics. i'm going to use this feature for the machine.
-    for index, f in enumerate(class_train_files):
-        audio, fs = librosa.load(os.path.join(train_root, f),
-                                 sr=None)  # first is the directory, the second f is the file
-        mfcc = compute_mfcc(audio, fs, n_mfcc)  # now I've a matrix of features.
-        train_features[index, :] = np.mean(mfcc, axis=1)
-        print(f)
+    train_mfcc_plot = np.zeros((n_train_samples, n_mfcc))
+    train_features = np.zeros((n_train_samples, n_feat_weights))
 
-    dict_train_features[
-        c] = train_features  # now we use a dictionary structure to save all features (is a sort of struct)
+    # we compute for each track: mfcc_mean, chroma_mean, spec_roll_mean and spec_centr_mean
+    for index, f in enumerate(class_train_files):
+        audio, fs = librosa.load(os.path.join(train_root, f), sr=None)
+        mfcc = compute_mfcc(audio, fs, n_mfcc)
+        mfcc_mean = np.mean(mfcc, axis=1)
+        chroma = librosa.feature.chroma_stft(audio, fs)
+        chroma_mean = np.mean(chroma, axis=1)
+        spec_roll = librosa.feature.spectral_rolloff(audio, fs)
+
+        ### Spectral Rolloff Plot
+
+        # X = np.abs(librosa.stft(
+        # audio,
+        # window='hamming',
+        # n_fft=1024, #number of points in which fft will be done in window.
+        # hop_length=512,)
+        # )
+
+        # rolloff = 0.99*spec_roll
+        # rolloff_min = 0.01*spec_roll
+
+        # fig, ax = plt.subplots()
+        # librosa.display.specshow(librosa.amplitude_to_db(X, ref=np.max), y_axis='log', x_axis='time', ax=ax)
+        # ax.plot(librosa.times_like(rolloff), rolloff[0], label='Roll-off frequency (0.99)')
+        # ax.plot(librosa.times_like(rolloff), rolloff_min[0], color='w', label='Roll-off frequency (0.01)')
+        # ax.legend(loc='lower right')
+        # ax.set(title='log Power spectrogram')
+
+        ## valid code but plots for library updates do not come out that we cannot modify (basey-> base; linscaley _> linscale)
+        ## to not have a thousand plots you can use the spec_roll_mean defined below instead of the spec_roll, outside this for loop
+
+        spec_roll_mean = np.mean(spec_roll, axis=1)
+        spec_centr = librosa.feature.spectral_centroid(audio, fs)
+        spec_centr_mean = np.mean(spec_centr, axis=1)
+        train_mfcc_plot[index, :] = np.mean(mfcc, axis=1)
+        train_features[index, :] = np.concatenate((mfcc_mean, chroma_mean, spec_roll_mean, spec_centr_mean), axis=0)
+    mfcc_plot[c] = train_mfcc_plot
+    dict_train_features[c] = train_features
+# print(dict_train_features[c].shape)
+
 
 # the syntax for test files is pretty the same.
 dict_test_features = {'rock': [], 'pop': [], 'reggae': [], 'disco': []}
 
 for c in classes:
-    test_root = 'D:/Documenti/SampleCanzoni/PreProcessed/Selected/{}/test/'.format(c)
+    test_root = '/Users/matteosartori/opt/miniconda3/CMLS/PreProcessedRed/{}/test'.format(c)
     class_test_files = [f for f in os.listdir(test_root) if f.endswith('.mp3')]
     n_test_samples = len(class_test_files)
 
-    test_features = np.zeros((n_test_samples, n_mfcc))
+    test_features = np.zeros((n_test_samples, n_feat_weights))
     for index, f in enumerate(class_test_files):
         audio, fs = librosa.load(os.path.join(test_root, f), sr=None)
         mfcc = compute_mfcc(audio, fs, n_mfcc)
-        test_features[index, :] = np.mean(mfcc, axis=1)
+        mfcc_mean = np.mean(mfcc, axis=1)
+        chroma = librosa.feature.chroma_stft(audio, fs)
+        chroma_mean = np.mean(chroma, axis=1)
+        spec_roll = librosa.feature.spectral_rolloff(audio, fs)
+        spec_roll_mean = np.mean(spec_roll, axis=1)
+        spec_centr = librosa.feature.spectral_centroid(audio, fs)
+        spec_centr_mean = np.mean(spec_centr, axis=1)
+        test_features[index, :] = np.concatenate((mfcc_mean, chroma_mean, spec_roll_mean, spec_centr_mean), axis=0)
     dict_test_features[c] = test_features
+    # print(dict_test_features[c].shape)
+
+
+# MFCC PLOT
 
 for c in classes:
-    mfcc = dict_train_features[c].transpose()
+    mfcc = mfcc_plot[c].transpose()
     # Visualization
     fig = plt.figure(figsize=(16, 6))
     plt.subplot(1,2,1)
@@ -113,6 +164,7 @@ y_train_3 = np.ones((X_train_3.shape[0],))*3
 
 #y_train = np.concatenate((y_train_class_0, y_train_class_1, y_train_class_1), axis=0)
 
+
 X_test_0 = dict_test_features[class_0]
 X_test_1 = dict_test_features[class_1]
 X_test_2 = dict_test_features[class_2]
@@ -137,11 +189,10 @@ X_train_3_normalized = (X_train_3 - feat_min) / (feat_max - feat_min)
 
 X_test_0_normalized = (X_test_0 - feat_min) / (feat_max - feat_min)
 X_test_1_normalized = (X_test_1 - feat_min) / (feat_max - feat_min)
-X_test_2_normalized = (X_test_2 - feat_min) / (feat_max - feat_min) #we concatenate to obtain max/min
+X_test_2_normalized = (X_test_2 - feat_min) / (feat_max - feat_min) # we concatenate to obtain max/min
 X_test_3_normalized = (X_test_3 - feat_min) / (feat_max - feat_min)
 
 X_test_mc_normalized = np.concatenate((X_test_0_normalized, X_test_1_normalized, X_test_2_normalized, X_test_3_normalized), axis=0)
-
 
 SVM_parameters={
     'C': 1,
@@ -176,7 +227,6 @@ clf_23.fit(np.concatenate((X_train_2_normalized, X_train_3_normalized), axis=0),
            np.concatenate((y_train_2, y_train_3), axis=0))
 # now we have a tree of model.
 
-
 #now we predict test on label. They're all binary classifiers. We have 3 prediction and we must extract the most present.
 y_test_predicted_01 = clf_01.predict(X_test_mc_normalized).reshape(-1, 1)
 y_test_predicted_02 = clf_02.predict(X_test_mc_normalized).reshape(-1, 1)
@@ -208,5 +258,5 @@ def compute_cm_multiclass(gt, predicted):
             CM[i, int(pred_class[j])] = CM[i, int(pred_class[j])] + 1
     print(CM)
 
-
 compute_cm_multiclass(y_test_mc, y_test_predicted_mv)
+
